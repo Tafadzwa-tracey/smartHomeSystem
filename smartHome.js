e60536f0cc75c9a619760e9ac126fae317a4e164
Tafadzwa Tracey Taruvinga <tafadzwatracey@gmail.com>
2019-12-26 14:16:56 +0800

    Added leave and back home mode

--- smartHome.js
+++ smartHome.js
@@ -1,12 +1,4 @@
-function on_ac(_this){
-  console.log('Ac pressed')
-  ws.send('AC ON')
-}
 
- function switchOn(_this){
-   ws.send(_this);
-
- }
   function setAlarm(_this) {
     _this.style.backgroundColor = "orange";
   }
@@ -38,15 +30,29 @@ function on_ac(_this){
       };
     }
     
+  
     function onSubmit() {
-      var input = document.getElementById("input");
+      var input = document.getElementById("input1");
       // You can send message to the Web Socket using ws.send.
-      ws.send(input.value);
+      ws.send("Alarm set:  " + input.value);
       //output("send: " + input.value);
       //input.value = "";
       //input.focus();
     }
     
+
+    function mySubmit() {
+      var input = document.getElementById("input");
+        //action for delete
+        ws.send("Appointment:  " + input.value);
+    }
+
+    function thySubmit() {
+      var input = document.getElementById("input3");
+        //action for delete
+        ws.send("Temperature set:  " + input.value);
+    }
+
     function onCloseClick() {
       ws.close();
     }
@@ -57,28 +63,26 @@ function on_ac(_this){
         replace(/>/, "&gt;").replace(/"/, "&quot;"); // "
       log.innerHTML = escaped + "<br>" + log.innerHTML;
     }
+
   $(document).ready(function(){
     $('.btn').click(function(){
       $('.btn').removeClass('active').addClass('inactive');
-       $(this).removeClass('inactive').addClass('active');
+      $(this).removeClass('inactive').addClass('active');
       });
   })
+
   $(document).ready(function(){
     $('.onBtn').click(function(){
-      ws.send('AC on')
+      ws.send('AC on');
       });
   })
   $(document).ready(function(){
     $('.offBtn').click(function(){
-      ws.send('AC off')
-      function onSubmit() {
-        var input = document.getElementById("input");
-        ws.send("Alarm set" + input.value);
-      }
-      
+      ws.send('AC off');
       });
   })
-  
+
+
   $(document).ready(function(){
     $('.btn1').click(function(){
       $('.btn1').removeClass('active').addClass('inactive');
@@ -87,12 +91,12 @@ function on_ac(_this){
   })
   $(document).ready(function(){
     $('.lightOn').click(function(){
-      ws.send('lights on')
+      ws.send('lights on');
       });
   })
   $(document).ready(function(){
     $('.lightOff').click(function(){
-      ws.send('lights off')
+      ws.send('lights off');
       });
   })
   $(document).ready(function(){
@@ -103,12 +107,12 @@ function on_ac(_this){
   })
   $(document).ready(function(){
     $('.floorOn').click(function(){
-      ws.send('Floor mopping robot on')
+      ws.send('Floor mopping robot on');
       });
   })
   $(document).ready(function(){
     $('.floorOff').click(function(){
-      ws.send('Floor mopping robot off')
+      ws.send('Floor mopping robot off');
       });
   })
   
@@ -122,13 +126,32 @@ function on_ac(_this){
 
   $(document).ready(function(){
     $('.onAlarm').click(function(){
-      ws.send('Alarm on')
-      });
+      ws.send('Alarm on');
+    });
   })
 
   
   $(document).ready(function(){
     $('.offAlarm').click(function(){
-      ws.send('Alarm off')
+      ws.send('Alarm off');
+      });
+  })
+
+  $(document).ready(function(){
+    $('.leaveHome').click(function(){
+      ws.send('Alarm off');
+      ws.send('Lights off');
+      ws.send('Floor mopping robot on');
+      ws.send('Air conditioner off')
+      });
+  })
+
+  $(document).ready(function(){
+    $('.backHome').click(function(){
+      ws.send('Alarm on');
+      ws.send('Lights on');
+      ws.send('Floor mopping robot off');
+      ws.send('Air conditioner off')
+      ws.send('Temperature: 25')
       });
   })
